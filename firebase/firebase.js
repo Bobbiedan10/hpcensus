f5c37f9a1948936b04bd9da7fba5dadecba6b7bd
Bobbiedan10 <dalrympleramon@gmail.com>
2022-02-08 20:19:19 -0400

    new commit

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 import Router from "next/router";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzExL2WYyl34E2ml8LPYuQ3ZZ4Ls4zuNY",
-  authDomain: "census2021-2022.firebaseapp.com",
-  databaseURL: "https://census2021-2022-default-rtdb.firebaseio.com",
-  projectId: "census2021-2022",
-  storageBucket: "census2021-2022.appspot.com",
-  messagingSenderId: "316664629063",
-  appId: "1:316664629063:web:a23206e2dffd483415ae62",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 class Firebase {
